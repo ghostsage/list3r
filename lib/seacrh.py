b62390f9351bf1b02f8a2def6e4590582b90c6e5
ghostsage <63196469+ghostsage@users.noreply.github.com>
2020-09-23 17:51:21 +0530

    Update seacrh.py

--- lib/seacrh.py
+++ lib/seacrh.py
@@ -1,32 +1,35 @@
 import requests
 from colorama import Fore
 
-class search:
-    def search(self,target,wordlist_type):
-        if wordlist_type=='small':
-            with open('wordlist/small.txt','r') as file:
-                for line in file:
-                    char=line.strip()
-                    url=target+'/'+ char
-                    r=requests.get(url)
-                    if r.status_code==200 or r.status_code==403:
-                        print(Fore.BLUE,'[+] Found:',url)
-            file.close()
-        elif wordlist_type=='medium':
-            with open('wordlist/medium.txt','r') as file:
-                for line in file:
-                    char=line.strip()
-                    url=target+'/'+ char
-                    r=requests.get(url)
-                    if r.status_code==200 or r.status_code==403:
-                        print(Fore.BLUE,'[+] Found:',url)   
-            file.close()                       
-        elif wordlist_type=='large':
-            with open('wordlist/large.txt','r') as file:
-                for line in file:
-                    char=line.strip()
-                    url=target+'/'+ char
-                    r=requests.get(url)
-                    if r.status_code==200 or r.status_code==403:
-                       print(Fore.BLUE,'[+] Found:',url)               
-            file.close()            
+try:
+    class search:
+        def search(self,target,wordlist_type):
+            if wordlist_type=='small':
+                with open('wordlist/small.txt','r') as file:
+                    for line in file:
+                        char=line.strip()
+                        url=target+'/'+ char
+                        r=requests.get(url)
+                        if r.status_code==200 or r.status_code==403:
+                            print(Fore.BLUE,'[+] Found:',url)
+                file.close()
+            elif wordlist_type=='medium':
+                with open('wordlist/medium.txt','r') as file:
+                    for line in file:
+                        char=line.strip()
+                        url=target+'/'+ char
+                        r=requests.get(url)
+                        if r.status_code==200 or r.status_code==403:
+                            print(Fore.BLUE,'[+] Found:',url)   
+                file.close()                       
+            elif wordlist_type=='large':
+                with open('wordlist/large.txt','r') as file:
+                    for line in file:
+                        char=line.strip()
+                        url=target+'/'+ char
+                        r=requests.get(url)
+                        if r.status_code==200 or r.status_code==403:
+                        print(Fore.BLUE,'[+] Found:',url)               
+                file.close()            
+except requests.exceptions.ConnectionError:
+    print(Fore.RED,'[!] Invalid Url!')
