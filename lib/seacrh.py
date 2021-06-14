8f9fd21bcf47a4358c005c93683ed6af273fef1c
ghostsage <63196469+ghostsage@users.noreply.github.com>
2021-06-14 17:16:36 +0530

    Update seacrh.py

--- lib/seacrh.py
+++ lib/seacrh.py
@@ -3,34 +3,18 @@ from colorama import Fore
 
 try:
     class search:
-        def search(self,target,wordlist_type):
-            if wordlist_type=='small':
-                with open('wordlist/small.txt','r') as file:
-                    for line in file:
-                        char=line.strip()
-                        url=target+'/'+ char
-                        r=requests.get(url)
-                        if r.status_code==200 or r.status_code==403:
-                            print(Fore.BLUE,'[+] Found:',url)
-                file.close()
-            elif wordlist_type=='medium':
-                with open('wordlist/medium.txt','r') as file:
-                    for line in file:
-                        char=line.strip()
-                        url=target+'/'+ char
-                        r=requests.get(url)
-                        if r.status_code==200 or r.status_code==403:
-                            print(Fore.BLUE,'[+] Found:',url)   
-                file.close()                       
-            elif wordlist_type=='large':
-                with open('wordlist/large.txt','r') as file:
-                    for line in file:
-                        char=line.strip()
-                        url=target+'/'+ char
-                        r=requests.get(url)
-                        if r.status_code==200 or r.status_code==403:
-                            print(Fore.BLUE,'[+] Found:',url)               
-                file.close()
+        def __init__(self,target):
+            self.target = target
+            self.search()
+        def search(self):                       
+            with open('wordlist/large.txt','r') as file:
+                for line in file:
+                    char=line.strip()
+                    url=self.target+'/'+ char
+                    r=requests.get(url)
+                    if r.status_code==200:
+                        print(Fore.BLUE,'[+] Found:',url)               
+                    file.close()
 except requests.exceptions.ConnectionError:
     print(Fore.RED,'[!] Please check your url!')
 except requests.exceptions.MissingSchema:
